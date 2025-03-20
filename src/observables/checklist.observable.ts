d1e48890c618d6a61d3102d90001767229975280
Justin Salcedo <nitsuj162114@gmail.com>
2025-03-19 21:21:10 -0700

    Hotfix: set checklist active state through an interval for reliability

--- src/observables/checklist.observable.ts
+++ src/observables/checklist.observable.ts
@@ -1,4 +1,4 @@
-import {computed, makeAutoObservable} from 'mobx'
+import {computed, makeAutoObservable, runInAction} from 'mobx'
 import {RootStore} from '../root.store'
 import {setAndCountVisibleTasks} from '../utils/task'
 
@@ -6,15 +6,32 @@ export class ChecklistObservable {
     searchTerm: string = ''
     showCompletedTasks: boolean = true
     showNonRequiredTasks: boolean = true
+    isActive: boolean = false
+    checkIsActiveInterval: NodeJS.Timer | number = 0
 
     constructor(private rootStore: RootStore) {
         makeAutoObservable(this, {
             allTasks: computed,
             visibleTasks: computed,
             loading: computed,
-            isActive: computed,
             isProcessed: computed,
         })
+
+        this.runCheckIsActiveInterval()
+    }
+
+    runCheckIsActiveInterval() {
+        this.checkIsActiveInterval = setInterval(() => {
+            runInAction(() => {
+                this.isActive =
+                    !!this.rootStore.checklistStore.todaysChecklist &&
+                    this.rootStore.checklistStore.todaysChecklist.endTimeInMs >
+                        Date.now()
+                if (!this.isActive) {
+                    clearInterval(this.checkIsActiveInterval)
+                }
+            })
+        }, 1000)
     }
 
     get allTasks() {
@@ -34,15 +51,6 @@ export class ChecklistObservable {
 
     get loading() {
         return !this.rootStore.checklistStore.synced
-    }
-
-    get isActive() {
-        return (
-            this.rootStore.checklistStore.todaysChecklist &&
-            (this.rootStore.checklistStore.todaysChecklist.endTimeInMs >
-                Date.now() ||
-                this.rootStore.checklistStore.todaysChecklist.processed)
-        )
     }
 
     get isProcessed() {
